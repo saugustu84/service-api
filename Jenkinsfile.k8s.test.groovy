c2d72b96d25f1ee055179e77741dd9fbf7ff4911
ihar-kahadouski <igor.kagadowski@gmail.com>
2019-12-20 12:54:29 +0300

    change reports font

--- Jenkinsfile.k8s.test.groovy
+++ Jenkinsfile.k8s.test.groovy
@@ -113,12 +113,7 @@ podTemplate(
             stage('Build Docker Image') {
                 dir(appDir) {
                     def buildParams = "-P sealightsToken=$sealightsToken -P sealightsSession=$sealightsSession -P buildNumber=$buildVersion"
-                    sh 'java -version'
-                    sh 'whoami'
-                    sh 'apk add --no-cache ca-certificates font-noto'
-                    sh 'mkdir /var/lib/postgresql && chown -R postgres /var/lib/postgresql'
-                    sh 'chown -R postgres .'
-                    sh "su - postgres -c 'cd $WORKSPACE/$appDir && ./gradlew createDockerfileDev $buildParams'"
+                    sh "./gradlew createDockerfileDev $buildParams"
                     container('docker') {
                         sh "cd ./build/libs"
                         sh "docker build -t $tag ."
